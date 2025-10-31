5485b2ec191bab7b7ef6f6ee06b940984d9fdb0d
Sonal <imsonalbajaj@gmail.com>
2025-10-31 22:01:29 +0530

    initial commit

---
+++ urVisa/urVisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  urVisaApp.swift
+//  urVisa
+//
+//  Created by Sonal on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct urVisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
