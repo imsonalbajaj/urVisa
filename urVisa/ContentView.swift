f9726c33b70faea5c34a6e2ddb4100e9eb9e43d6
Sonal <imsonalbajaj@gmail.com>
2025-11-02 13:43:49 +0530

    carousel added

--- urVisa/ContentView.swift
+++ urVisa/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Sample carousel items - easily extensible
+    // To add more items, just add to this array - takes less than 15 mins!
+    let carouselItems: [CarouselItem] = [
+        CarouselItem(imageName: "img1", contryName: "Italy"),
+        CarouselItem(imageName: "img2", contryName: "France"),
+        CarouselItem(imageName: "img3", contryName: "Germany"),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 8) {
+            Text("Atlas")
+                .font(.largeTitle)
+                .foregroundStyle(Color.black)
+                .padding(.top, 48.0)
+            
+            Text("Vias on Time")
+                .font(.caption)
+                .foregroundStyle(Color.gray)
+                .padding(.bottom, 18.0)
+            
+            CarouselView(items: carouselItems)
+                .padding(32.0)
+            
+            Spacer()
+                .frame(height: 200)
         }
-        .padding()
     }
 }
 
